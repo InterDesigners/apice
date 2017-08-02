d979d5dbc796343a6fe31e8da2ec477bfa656d84
Digoritos <rmc.morelli@gmail.com>
2017-08-02 00:17:45 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1501520083,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/brick-sheet0.png",
+		"images/brick-sheet1.png",
+		"images/brick-sheet2.png",
+		"images/brickspawner-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/bgfront.png",
+		"images/bgback.png",
+		"images/bggradient-sheet0.png",
+		"images/death-sheet0.png",
+		"images/wall.png",
+		"images/brickbase-sheet0.png",
+		"images/bomb-sheet0.png",
+		"images/bombspawner-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
